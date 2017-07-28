2f3d532c02eb44835349b004fac42d0262e73d5b
Adam Mann <manny160389@hotmail.com>
2017-07-28 13:07:03 +0100

    Initial commit

---
+++ chocobros.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if request.form['card_name'] :
+            return redirect(url_for('search') + '/' + request.form['card_name'])
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/search')
+@app.route('/search/<card_name>')
+def search(card_name=None):
+    return render_template('search.html', card_name=card_name)
